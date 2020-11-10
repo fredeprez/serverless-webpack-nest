e7777e3c791ed9bad2db47f1da0e9dc369928d0e
Karel Verschraegen <karel@cheqroom.com>
2020-11-10 10:15:37 +0100

    :arrow_down: Downgrade fastify dependency

--- services/hello/hello-handler.ts
+++ services/hello/hello-handler.ts
@@ -2,6 +2,7 @@ import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import * as proxy from 'aws-lambda-fastify';
+import * as fastify from 'fastify';
 
 import { HelloModule } from './hello-module';
 
@@ -9,7 +10,7 @@ let app: INestApplication;
 
 export const handler = async (event, context) => {
 	if (!app) {
-		app = await NestFactory.create<NestFastifyApplication>(HelloModule, new FastifyAdapter());
+		app = await NestFactory.create<NestFastifyApplication>(HelloModule, new FastifyAdapter(fastify()));
 		await app.init();
 	}
 
